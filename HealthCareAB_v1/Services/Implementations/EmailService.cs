44a1e3ba7c6efe9d1170b35e36e012254aea5dad
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-05 19:24:58 +0100

    add a service without implementation details

---
+++ HealthCareAB_v1/Services/Implementations/EmailService.cs
@@ -0,0 +1,11 @@
+using HealthCareAB_v1.Services.Interfaces;
+
+namespace HealthCareAB_v1.Services.Implementations;
+
+public class EmailService : IEmailService
+{
+    public Task SendEmailAsync(IEmailService.Options options)
+    {
+        throw new NotImplementedException();
+    }
+}
