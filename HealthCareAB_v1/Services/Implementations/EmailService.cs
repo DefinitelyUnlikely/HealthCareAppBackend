bc281cdef7d5180863dec3884a421a8587bc577d
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-06 01:01:35 +0100

    add SendEmailAsync code

--- HealthCareAB_v1/Services/Implementations/EmailService.cs
+++ HealthCareAB_v1/Services/Implementations/EmailService.cs
@@ -1,18 +1,36 @@
 using HealthCareAB_v1.Services.Interfaces;
+using MailKit.Net.Smtp;
 using MimeKit;
+using MimeKit.Text;
 
 namespace HealthCareAB_v1.Services.Implementations;
 
 public class AzureEmailService : IEmailService
 {
-    public Task SendEmailAsync(IEmailService.Email email)
+    public async Task SendEmailAsync(IEmailService.Email email)
     {
         var message = new MimeMessage();
         message.From.Add(new MailboxAddress("HealthCareAB", "healthcareab@outlook.com"));
         message.To.Add(new MailboxAddress(email.To, email.To));
         message.Subject = email.Subject;
-        message.Body = new TextPart("html") { Text = email.HtmlContent };
 
-        return Task.CompletedTask;
+        if (email.HtmlContent is not null)
+        {
+            var bodyBuilder = new BodyBuilder
+            {
+                HtmlBody = $"{email.HtmlContent}",
+                TextBody = $"{email.PlainContent}"
+            };
+            message.Body = bodyBuilder.ToMessageBody();
+        }
+        else
+        {
+            message.Body = new TextPart(TextFormat.Plain) { Text = email.PlainContent };
+        }
+
+        using var smtpClient = new SmtpClient();
+        await smtpClient.ConnectAsync(""); // add server settings using env variables.
+        await smtpClient.SendAsync(message);
+        await smtpClient.DisconnectAsync(true);
     }
 }
