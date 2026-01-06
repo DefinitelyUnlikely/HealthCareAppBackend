066d38a8bd4610786ed1c9bc437fee9966e4b5fa
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-06 10:01:57 +0100

    start arranging test dependencies

--- HealthCareApp.Tests/EmailServiceTests.cs
+++ HealthCareApp.Tests/EmailServiceTests.cs
@@ -1,9 +1,31 @@
 using Moq;
 using HealthCareAB_v1.Services.Interfaces;
 using HealthCareAB_v1.Services.Implementations;
+using Microsoft.Extensions.Configuration;
 
 namespace HealthCareApp.Tests;
 
-class EmailServiceTests
+public class EmailServiceTests
 {
+    [Fact]
+    public void SendEmailAsync_WhenCalled_SendsEmail()
+    {
+        // Arrange
+        var inMemoryConfig = new Dictionary<string, string>
+        {
+            { "SMTP:Host", "localhost" },
+            { "SMTP:Port", "1025" }
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemoryConfig)
+            .Build();
+
+        var emailService = new AzureEmailService(configuration);
+
+        // Act
+
+
+        // Assert
+    }
 }
