80526bfa37e003e05666f483780f5c13b1006e20
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-06 17:46:12 +0100

    make notification service outline

---
+++ HealthCareAB_v1/Models/Notification.cs
@@ -0,0 +1,16 @@
+public class Notification
+{
+}
+
+public class ConfirmationNotification : Notification
+{
+}
+
+public class CancelNotification : Notification
+{
+}
+
+public class UpdateNotification : Notification
+{
+}
+
---
+++ HealthCareAB_v1/Services/Implementations/NotificationService.cs
@@ -0,0 +1,12 @@
+using HealthCareAB_v1.Models;
+using HealthCareAB_v1.Services.Interfaces;
+
+namespace HealthCareAB_v1.Services.Implementations;
+
+public class NotificationService : INotificationService
+{
+    public Task SendNotificationAsync(Notification notification)
+    {
+        throw new NotImplementedException();
+    }
+}
