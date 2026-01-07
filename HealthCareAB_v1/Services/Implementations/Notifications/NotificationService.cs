be4de8d411c420d23d2cba0f60c8080a2b9ca4db
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-07 19:07:38 +0100

    add a roadmap for my own visualization

--- HealthCareAB_v1/Services/Implementations/Notifications/NotificationService.cs
+++ HealthCareAB_v1/Services/Implementations/Notifications/NotificationService.cs
@@ -7,6 +7,10 @@ public class NotificationService : INotificationService
 {
     public Task SendNotificationAsync(Notification notification)
     {
+        // What needs to be done here?
+        // 1. Check through all handlers, to see if any of them can handle the notification
+        // 2. If a handler can handle the notification, call the handler
+        // 3. If no handler can handle the notification, throw an exception
         throw new NotImplementedException();
     }
 }
