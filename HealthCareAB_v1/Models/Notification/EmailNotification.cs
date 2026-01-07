c944a5fffc35c3d06da3b0867e9235a34e018bb3
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-07 18:50:39 +0100

    add proper message and html content, make those strings none required and add default values instead.

--- HealthCareAB_v1/Models/Notification/EmailNotification.cs
+++ HealthCareAB_v1/Models/Notification/EmailNotification.cs
@@ -3,7 +3,7 @@ namespace HealthCareAB_v1.Models.Notification;
 public class EmailNotification : Notification
 {
     public virtual required string Subject { get; set; }
-    public virtual required string Html { get; set; }
+    public virtual string Html { get; set; } = string.Empty;
 }
 
 public class MeetingConfirmedEmailNotification : EmailNotification
@@ -11,8 +11,11 @@ public class MeetingConfirmedEmailNotification : EmailNotification
     public override required string Subject { get; set; } = "Möte bokat";
     public required Meeting Meeting { get; set; }
 
-    public override required string Message { get; set; } = "Möte bokat";
-    public override required string Html { get; set; } = "Möte bokat";
+    public override string Message =>
+        $"Hej, Möte bokat {Meeting.StartTime} på {Meeting.Caregiver.Address}. Hälsningar HealthCare AB";
+
+    public override string Html =>
+        $"<p>Hej,</p><p>Möte bokat {Meeting.StartTime} på {Meeting.Caregiver.Address}</p><p>Hälsningar HealthCare AB</p>";
 }
 
 public class MeetingCancelledEmailNotification : EmailNotification
@@ -20,8 +23,11 @@ public class MeetingCancelledEmailNotification : EmailNotification
     public override required string Subject { get; set; } = "Möte avbokat";
     public required Meeting Meeting { get; set; }
 
-    public override required string Message { get; set; } = "Möte avbokat";
-    public override required string Html { get; set; } = "Möte avbokat";
+    public override string Message =>
+        $"Hej, Möte vid {Meeting.StartTime} på {Meeting.Caregiver.Address} avbokat. Hälsningar HealthCare AB";
+
+    public override string Html =>
+        $"<p>Hej,</p><p>Möte vid {Meeting.StartTime} på {Meeting.Caregiver.Address} avbokat</p><p>Hälsningar HealthCare AB</p>";
 }
 
 public class MeetingReminderEmailNotification : EmailNotification
@@ -29,8 +35,11 @@ public class MeetingReminderEmailNotification : EmailNotification
     public override required string Subject { get; set; } = "Mötespåminnelse";
     public required Meeting Meeting { get; set; }
 
-    public override required string Message { get; set; } = "Mötespåminnelse";
-    public override required string Html { get; set; } = "Mötespåminnelse";
+    public override string Message =>
+        $"Hej, Mötespåminnelse för möte vid {Meeting.StartTime} på {Meeting.Caregiver.Address}. Hälsningar HealthCare AB";
+
+    public override string Html =>
+        $"<p>Hej,</p><p>Mötespåminnelse för möte vid {Meeting.StartTime} på {Meeting.Caregiver.Address}</p><p>Hälsningar HealthCare AB</p>";
 }
 
 public class MeetingUpdatedEmailNotification : EmailNotification
@@ -39,6 +48,9 @@ public class MeetingUpdatedEmailNotification : EmailNotification
     public required Meeting OldMeeting { get; set; }
     public required Meeting NewMeeting { get; set; }
 
-    public override required string Message { get; set; } = "Möte uppdaterat";
-    public override required string Html { get; set; } = "Möte uppdaterat";
+    public override string Message =>
+        $"Hej, Möte uppdaterat {OldMeeting.StartTime} till {NewMeeting.StartTime} på {NewMeeting.Caregiver.Address}. Hälsningar HealthCare AB";
+
+    public override string Html =>
+        $"<p>Hej,</p><p>Möte uppdaterat {OldMeeting.StartTime} till {NewMeeting.StartTime} på {NewMeeting.Caregiver.Address}</p><p>Hälsningar HealthCare AB</p>";
 }
--- HealthCareAB_v1/Models/Notification/Notification.cs
+++ HealthCareAB_v1/Models/Notification/Notification.cs
@@ -3,6 +3,6 @@ namespace HealthCareAB_v1.Models.Notification;
 public class Notification
 {
     public virtual required User SendToUser { get; set; }
-    public virtual required string Message { get; set; }
+    public virtual string Message { get; set; } = string.Empty;
 }
 
