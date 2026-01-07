bb7ef1c7c93e3cab63c97a7d9f5d9c40f7187f3d
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-07 18:35:51 +0100

    update 'parent' notifications to use virtual strings for overideability + add some placeholer message and html strings

--- HealthCareAB_v1/Models/Notification/EmailNotification.cs
+++ HealthCareAB_v1/Models/Notification/EmailNotification.cs
@@ -2,27 +2,43 @@ namespace HealthCareAB_v1.Models.Notification;
 
 public class EmailNotification : Notification
 {
-    public required string Subject { get; set; }
-    public required string Html { get; set; }
+    public virtual required string Subject { get; set; }
+    public virtual required string Html { get; set; }
 }
 
 public class MeetingConfirmedEmailNotification : EmailNotification
 {
+    public override required string Subject { get; set; } = "Möte bokat";
     public required Meeting Meeting { get; set; }
+
+    public override required string Message { get; set; } = "Möte bokat";
+    public override required string Html { get; set; } = "Möte bokat";
 }
 
 public class MeetingCancelledEmailNotification : EmailNotification
 {
+    public override required string Subject { get; set; } = "Möte avbokat";
     public required Meeting Meeting { get; set; }
+
+    public override required string Message { get; set; } = "Möte avbokat";
+    public override required string Html { get; set; } = "Möte avbokat";
 }
 
 public class MeetingReminderEmailNotification : EmailNotification
 {
+    public override required string Subject { get; set; } = "Mötespåminnelse";
     public required Meeting Meeting { get; set; }
+
+    public override required string Message { get; set; } = "Mötespåminnelse";
+    public override required string Html { get; set; } = "Mötespåminnelse";
 }
 
 public class MeetingUpdatedEmailNotification : EmailNotification
 {
+    public override required string Subject { get; set; } = "Möte uppdaterat";
     public required Meeting OldMeeting { get; set; }
     public required Meeting NewMeeting { get; set; }
+
+    public override required string Message { get; set; } = "Möte uppdaterat";
+    public override required string Html { get; set; } = "Möte uppdaterat";
 }
--- HealthCareAB_v1/Models/Notification/Notification.cs
+++ HealthCareAB_v1/Models/Notification/Notification.cs
@@ -2,7 +2,7 @@ namespace HealthCareAB_v1.Models.Notification;
 
 public class Notification
 {
-    public required User SendToUser { get; set; }
-    public required string Message { get; set; }
+    public virtual required User SendToUser { get; set; }
+    public virtual required string Message { get; set; }
 }
 
