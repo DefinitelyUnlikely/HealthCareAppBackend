1a4506b1a5c8a7c35e8d2b4a54781a98d375a609
oozet <oozet@users.noreply.github.com>
2026-01-08 10:38:09 +0100

    fix name mismatch

--- HealthCareAB_v1/Services/Interfaces/IMeetingService.cs
+++ HealthCareAB_v1/Services/Interfaces/IMeetingService.cs
@@ -5,7 +5,7 @@ namespace HealthCareAB_v1.Services.Interfaces;
 
 public interface IMeetingService
 {
-    public Task<Meeting> CreateAsync(CreateMeetingDto meeting);
-    public Task<Meeting> ConfirmAsync(ConfirmMeetingDto meeting);
+    public Task<Meeting> CreateAsync(CreateMeetingDto request);
+    public Task<Meeting> ConfirmAsync(ConfirmMeetingDto request);
     public Task<MeetingResponseDto> GetMeetingAsync(Guid id, int userId, bool isAdmin);
 }
