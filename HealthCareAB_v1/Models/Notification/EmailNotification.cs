6dca4b0cb9c016a542598c6dd6abd8d2f99fcd23
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-09 12:23:09 +0100

    add a new way to do messages, we're already making this model less model and more class anyway

--- HealthCareAB_v1/Models/Notification/EmailNotification.cs
+++ HealthCareAB_v1/Models/Notification/EmailNotification.cs
@@ -11,11 +11,13 @@ public class MeetingConfirmedEmailNotification : EmailNotification
     public override required string Subject { get; set; } = "Möte bokat";
     public required Meeting Meeting { get; set; }
 
+    public string AddressMessage => Meeting.Caregiver != null ? " på " + Meeting.Caregiver.Address : string.Empty;
+
     public override string Message =>
-        $"Hej, Möte bokat {Meeting.StartTime} på {Meeting.Caregiver?.Address}. Hälsningar HealthCare AB";
+        $"Hej, Möte bokat {Meeting.StartTime}{AddressMessage}. Hälsningar HealthCare AB";
 
     public override string Html =>
-        $"<p>Hej,</p><p>Möte bokat {Meeting.StartTime} på {Meeting.Caregiver?.Address}</p><p>Hälsningar HealthCare AB</p>";
+        $"<p>Hej,</p><p>Möte bokat {Meeting.StartTime}{AddressMessage}</p><p>Hälsningar HealthCare AB</p>";
 }
 
 public class MeetingCancelledEmailNotification : EmailNotification
@@ -23,11 +25,13 @@ public class MeetingCancelledEmailNotification : EmailNotification
     public override required string Subject { get; set; } = "Möte avbokat";
     public required Meeting Meeting { get; set; }
 
+    public string AddressMessage => Meeting.Caregiver != null ? " på " + Meeting.Caregiver.Address : string.Empty;
+
     public override string Message =>
-        $"Hej, Möte vid {Meeting.StartTime} på {Meeting.Caregiver?.Address} avbokat. Hälsningar HealthCare AB";
+        $"Hej, Möte vid {Meeting.StartTime}{AddressMessage} avbokat. Hälsningar HealthCare AB";
 
     public override string Html =>
-        $"<p>Hej,</p><p>Möte vid {Meeting.StartTime} på {Meeting.Caregiver?.Address} avbokat</p><p>Hälsningar HealthCare AB</p>";
+        $"<p>Hej,</p><p>Möte vid {Meeting.StartTime}{AddressMessage} avbokat</p><p>Hälsningar HealthCare AB</p>";
 }
 
 public class MeetingReminderEmailNotification : EmailNotification
@@ -35,11 +39,13 @@ public class MeetingReminderEmailNotification : EmailNotification
     public override required string Subject { get; set; } = "Mötespåminnelse";
     public required Meeting Meeting { get; set; }
 
+    public string AddressMessage => Meeting.Caregiver != null ? " på " + Meeting.Caregiver.Address : string.Empty;
+
     public override string Message =>
-        $"Hej, Mötespåminnelse för möte vid {Meeting.StartTime} på {Meeting.Caregiver?.Address}. Hälsningar HealthCare AB";
+        $"Hej, Mötespåminnelse för möte vid {Meeting.StartTime}{AddressMessage}. Hälsningar HealthCare AB";
 
     public override string Html =>
-        $"<p>Hej,</p><p>Mötespåminnelse för möte vid {Meeting.StartTime} på {Meeting.Caregiver?.Address}</p><p>Hälsningar HealthCare AB</p>";
+        $"<p>Hej,</p><p>Mötespåminnelse för möte vid {Meeting.StartTime}{AddressMessage}</p><p>Hälsningar HealthCare AB</p>";
 }
 
 public class MeetingUpdatedEmailNotification : EmailNotification
@@ -48,9 +54,11 @@ public class MeetingUpdatedEmailNotification : EmailNotification
     public required Meeting OldMeeting { get; set; }
     public required Meeting NewMeeting { get; set; }
 
+    public string AddressMessage => NewMeeting.Caregiver != null ? " på " + NewMeeting.Caregiver.Address : string.Empty;
+
     public override string Message =>
-    $"Hej, Möte uppdaterat {OldMeeting.StartTime} till {NewMeeting.StartTime} på {NewMeeting.Caregiver?.Address}. Hälsningar HealthCare AB";
+        $"Hej, Möte uppdaterat från {OldMeeting.StartTime} till {NewMeeting.StartTime}{AddressMessage}. Hälsningar HealthCare AB";
 
     public override string Html =>
-        $"<p>Hej,</p><p>Möte uppdaterat {OldMeeting.StartTime} till {NewMeeting.StartTime} på {NewMeeting.Caregiver?.Address}</p><p>Hälsningar HealthCare AB</p>";
+        $"<p>Hej,</p><p>Möte uppdaterat från {OldMeeting.StartTime} till {NewMeeting.StartTime}{AddressMessage}</p><p>Hälsningar HealthCare AB</p>";
 }
--- HealthCareApp.Tests/AuthTests.cs
+++ HealthCareApp.Tests/AuthTests.cs
@@ -10,147 +10,146 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Moq;
 
-namespace HealthCareApp.Tests
+namespace HealthCareApp.Tests;
+
+public class LogoutTests
 {
-    public class LogoutTests
+    [Fact]
+    public void LogoutEndpoint_CallsAuthServiceGetClearCookieOptions()
     {
-        [Fact]
-        public void LogoutEndpoint_CallsAuthServiceGetClearCookieOptions()
+        // Arrange
+        var authServiceMock = new Mock<IAuthService>();
+        authServiceMock.Setup(s => s.GetClearCookieOptions()).Returns(new CookieOptions
         {
-            // Arrange
-            var authServiceMock = new Mock<IAuthService>();
-            authServiceMock.Setup(s => s.GetClearCookieOptions()).Returns(new CookieOptions
-            {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict,
-                Path = "/",
-                Expires = DateTimeOffset.UtcNow.AddDays(-1)
-            });
-            var controller = new AuthController(authServiceMock.Object)
-            {
-                ControllerContext = new ControllerContext
-                {
-                    HttpContext = new DefaultHttpContext
-                    {
-                        User = new ClaimsPrincipal(new ClaimsIdentity()) // no claims
-                    }
-                }
-            };
-
-
-            // Act
-            controller.Logout();
-
-            // Assert
-            authServiceMock.Verify(s => s.GetClearCookieOptions(), Times.Once());
-        }
-
-        [Fact]
-        public void LogoutEndpoint_ReturnsOkObjectResult_WhenCalled()
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Path = "/",
+            Expires = DateTimeOffset.UtcNow.AddDays(-1)
+        });
+        var controller = new AuthController(authServiceMock.Object)
         {
-            // Arrange
-            var authServiceMock = new Mock<IAuthService>();
-            authServiceMock.Setup(s => s.GetClearCookieOptions()).Returns(new CookieOptions
+            ControllerContext = new ControllerContext
             {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict,
-                Path = "/",
-                Expires = DateTimeOffset.UtcNow.AddDays(-1)
-            });
-            var controller = new AuthController(authServiceMock.Object)
-            {
-                ControllerContext = new ControllerContext
+                HttpContext = new DefaultHttpContext
                 {
-                    HttpContext = new DefaultHttpContext
-                    {
-                        User = new ClaimsPrincipal(new ClaimsIdentity()) // no claims
-                    }
+                    User = new ClaimsPrincipal(new ClaimsIdentity()) // no claims
                 }
-            };
+            }
+        };
 
-            // Act
-            var result = controller.Logout();
 
-            // Assert
-            Assert.IsType<OkObjectResult>(result);
-        }
+        // Act
+        controller.Logout();
 
-        [Fact]
-        public void LogoutEndpoint_RemovesCookie_WhenCalled()
-        {
-            // Arrange
-            var authServiceMock = new Mock<IAuthService>();
-            authServiceMock.Setup(s => s.GetClearCookieOptions()).Returns(new CookieOptions
-            {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict,
-                Path = "/",
-                Expires = DateTimeOffset.UtcNow.AddDays(-1)
-            });
-
-            // Asked the antigravity agent for help with how to set and find cookies in a unit test. 
-            var httpContext = new DefaultHttpContext();
-            var requestCookiesMock = new Mock<IRequestCookieCollection>();
-            requestCookiesMock.Setup(c => c.ContainsKey(CookieNames.Jwt)).Returns(true);
-            requestCookiesMock.Setup(c => c[CookieNames.Jwt]).Returns("test-token");
-            httpContext.Request.Cookies = requestCookiesMock.Object;
-
-            var controller = new AuthController(authServiceMock.Object)
-            {
-                ControllerContext = new ControllerContext
-                {
-                    HttpContext = httpContext
-                }
-            };
-
-            // Act
-            controller.Logout();
-
-            // Assert
-            var setCookieHeader = httpContext.Response.Headers.SetCookie;
-            Assert.True(setCookieHeader.Count > 0);
-            var cookieString = setCookieHeader.ToString();
-            Assert.Contains($"{CookieNames.Jwt}=", cookieString);
-            Assert.Contains("expires=", cookieString);
-        }
+        // Assert
+        authServiceMock.Verify(s => s.GetClearCookieOptions(), Times.Once());
     }
 
-    public class AuthServiceTests
+    [Fact]
+    public void LogoutEndpoint_ReturnsOkObjectResult_WhenCalled()
     {
-        [Fact]
-        public void GetClearCookieOptions_ReturnsCorrectOptions()
+        // Arrange
+        var authServiceMock = new Mock<IAuthService>();
+        authServiceMock.Setup(s => s.GetClearCookieOptions()).Returns(new CookieOptions
         {
-            // Arrange
-            var userServiceMock = new Mock<IUserService>();
-            var jwtTokenServiceMock = new Mock<IJwtTokenService>();
-            var jwtSettingsMock = new Mock<IOptions<JwtSettings>>();
-            var environmentMock = new Mock<IWebHostEnvironment>();
-            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Path = "/",
+            Expires = DateTimeOffset.UtcNow.AddDays(-1)
+        });
+        var controller = new AuthController(authServiceMock.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity()) // no claims
+                }
+            }
+        };
 
-            jwtSettingsMock.Setup(s => s.Value).Returns(new JwtSettings());
-            environmentMock.Setup(e => e.EnvironmentName).Returns("Production");
+        // Act
+        var result = controller.Logout();
 
-            var authService = new AuthService(
-                userServiceMock.Object,
-                jwtTokenServiceMock.Object,
-                jwtSettingsMock.Object,
-                environmentMock.Object,
-                httpContextAccessorMock.Object);
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
+    }
 
-            // Act
-            var options = authService.GetClearCookieOptions();
+    [Fact]
+    public void LogoutEndpoint_RemovesCookie_WhenCalled()
+    {
+        // Arrange
+        var authServiceMock = new Mock<IAuthService>();
+        authServiceMock.Setup(s => s.GetClearCookieOptions()).Returns(new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Path = "/",
+            Expires = DateTimeOffset.UtcNow.AddDays(-1)
+        });
 
-            // Assert
-            Assert.True(options.HttpOnly);
-            Assert.True(options.Secure);
-            Assert.Equal(SameSiteMode.Strict, options.SameSite);
-            Assert.Equal("/", options.Path);
-            Assert.NotNull(options.Expires);
-            // Just assert that expires is in the past, exact time is not important and harder to test.
-            Assert.True(options.Expires < DateTimeOffset.UtcNow);
-        }
+        // Asked the antigravity agent for help with how to set and find cookies in a unit test. 
+        var httpContext = new DefaultHttpContext();
+        var requestCookiesMock = new Mock<IRequestCookieCollection>();
+        requestCookiesMock.Setup(c => c.ContainsKey(CookieNames.Jwt)).Returns(true);
+        requestCookiesMock.Setup(c => c[CookieNames.Jwt]).Returns("test-token");
+        httpContext.Request.Cookies = requestCookiesMock.Object;
+
+        var controller = new AuthController(authServiceMock.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            }
+        };
+
+        // Act
+        controller.Logout();
+
+        // Assert
+        var setCookieHeader = httpContext.Response.Headers.SetCookie;
+        Assert.True(setCookieHeader.Count > 0);
+        var cookieString = setCookieHeader.ToString();
+        Assert.Contains($"{CookieNames.Jwt}=", cookieString);
+        Assert.Contains("expires=", cookieString);
+    }
+}
+
+public class AuthServiceTests
+{
+    [Fact]
+    public void GetClearCookieOptions_ReturnsCorrectOptions()
+    {
+        // Arrange
+        var userServiceMock = new Mock<IUserService>();
+        var jwtTokenServiceMock = new Mock<IJwtTokenService>();
+        var jwtSettingsMock = new Mock<IOptions<JwtSettings>>();
+        var environmentMock = new Mock<IWebHostEnvironment>();
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+        jwtSettingsMock.Setup(s => s.Value).Returns(new JwtSettings());
+        environmentMock.Setup(e => e.EnvironmentName).Returns("Production");
+
+        var authService = new AuthService(
+            userServiceMock.Object,
+            jwtTokenServiceMock.Object,
+            jwtSettingsMock.Object,
+            environmentMock.Object,
+            httpContextAccessorMock.Object);
+
+        // Act
+        var options = authService.GetClearCookieOptions();
+
+        // Assert
+        Assert.True(options.HttpOnly);
+        Assert.True(options.Secure);
+        Assert.Equal(SameSiteMode.Strict, options.SameSite);
+        Assert.Equal("/", options.Path);
+        Assert.NotNull(options.Expires);
+        // Just assert that expires is in the past, exact time is not important and harder to test.
+        Assert.True(options.Expires < DateTimeOffset.UtcNow);
     }
 }
