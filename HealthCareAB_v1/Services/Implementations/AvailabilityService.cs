f7854ca61ebd9ce8008d9316c6110b21c3109dd6
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-14 22:55:45 +0100

    update with imports and correct return type

--- HealthCareAB_v1/Services/Implementations/AvailabilityService.cs
+++ HealthCareAB_v1/Services/Implementations/AvailabilityService.cs
@@ -1,3 +1,4 @@
+using HealthCareAB_v1.Models;
 using HealthCareAB_v1.Repositories.Interfaces;
 using HealthCareAB_v1.Services.Interfaces;
 
@@ -7,18 +8,19 @@ public class AvailabilityService(IAvailabilityRepository availabilityRepository)
 {
     // Assuming we are available by default, this method, somewhat ironically,
     // calls the delete operation from the repository. 
-    public Task SetAvailableAsync(int userId, DateTime? from = null, DateTime? to = null)
+    public async Task SetAvailableAsync(int userId, DateTime? from = null, DateTime? to = null)
     {
         throw new NotImplementedException();
     }
 
-    public Task SetUnavailableAsync(int userId, DateTime? from = null, DateTime? to = null, bool forceCancel = false)
+    public async Task SetUnavailableAsync(int userId, DateTime? from = null, DateTime? to = null,
+        bool forceCancel = false)
     {
         throw new NotImplementedException();
     }
 
-    public Task GetUnavailabilityAsync(int userId, DateTime? from = null, DateTime? to = null)
+    public async Task<List<Availability>> GetUnavailabilityAsync(int userId, DateTime? from = null, DateTime? to = null)
     {
-        return availabilityRepository.GetUnavailabilityAsync(userId, from, to);
+        return await availabilityRepository.GetUnavailabilityAsync(userId, from, to);
     }
 }
--- HealthCareAB_v1/Services/Interfaces/IAvailabilityService.cs
+++ HealthCareAB_v1/Services/Interfaces/IAvailabilityService.cs
@@ -1,8 +1,10 @@
+using HealthCareAB_v1.Models;
+
 namespace HealthCareAB_v1.Services.Interfaces;
 
 public interface IAvailabilityService
 {
     public Task SetAvailableAsync(int userId, DateTime? from = null, DateTime? to = null);
     public Task SetUnavailableAsync(int userId, DateTime? from = null, DateTime? to = null, bool forceCancel = false);
-    public Task GetUnavailabilityAsync(int userId, DateTime? from = null, DateTime? to = null);
+    public Task<List<Availability>> GetUnavailabilityAsync(int userId, DateTime? from = null, DateTime? to = null);
 }
