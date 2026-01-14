0391015139b164eced88059ce09d0234b042264b
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-14 22:32:56 +0100

    add my thoughts on how to get availability from context and add the context injection

--- HealthCareAB_v1/Repositories/Implementations/AvailabilityRepository.cs
+++ HealthCareAB_v1/Repositories/Implementations/AvailabilityRepository.cs
@@ -2,8 +2,10 @@ using HealthCareAB_v1.Repositories.Interfaces;
 
 namespace HealthCareAB_v1.Repositories.Implementations;
 
-public class AvailabilityRepository : IAvailabilityRepository
+public class AvailabilityRepository(IAppDbContext context) : IAvailabilityRepository
 {
+    private readonly IAppDbContext _context = context ?? throw new ArgumentNullException(nameof(context));
+
     public Task SaveAvailabilityAsync(int userId, DateTime? from, DateTime? to)
     {
         throw new NotImplementedException();
@@ -11,6 +13,12 @@ public class AvailabilityRepository : IAvailabilityRepository
 
     public Task GetAvailabilityAsync(int userId, DateTime? from, DateTime? to)
     {
+        // How do I figure this one to work?
+        // My first thought is simply taking the dates and 
+        // then creating a query that returns all availabilities.
+        // We add a where statement to only return entitites that have start and end dates 
+        // within the range of the dates provided.
+        // and another where statement to only return entities that have the user id provided.
         throw new NotImplementedException();
     }
 }
