f3fdd1c3f5af3b5643ab022a403ee7afa01b6764
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-01-19 23:08:43 +0100

    change to primary constructor

--- HealthCareAB_v1/Controllers/AvailabilityController.cs
+++ HealthCareAB_v1/Controllers/AvailabilityController.cs
@@ -3,18 +3,13 @@ using HealthCareAB_v1.DTOs.Availability;
 using HealthCareAB_v1.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
+namespace HealthCareAB_v1.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
 [Produces("application/json")]
-public class AvailabilityController : ControllerBase
+public class AvailabilityController(IAvailabilityService availabilityService) : ControllerBase
 {
-    private readonly IAvailabilityService _availabilityService;
-
-    public AvailabilityController(IAvailabilityService availabilityService)
-    {
-        _availabilityService = availabilityService;
-    }
-
     [HttpGet]
     [Route("get-availability")]
     [ProducesResponseType(StatusCodes.Status200OK)]
